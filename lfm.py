c04862187a90a62890efe8380cd1f96823090426
Jacob Siegelman <jacob.siegelman1@gmail.com>
2021-07-19 13:05:46 -0700

    Create lfm.py

---
+++ lfm.py
@@ -0,0 +1,22 @@
+import sys
+import json
+albums = []
+for line in sys.stdin:
+	j = json.loads(line)
+	for a in j['topalbums']['album']:
+		albums.append('<!-- lfm -->' + a['name'] + ' by ' + a['artist']['name'] + '\n\n')
+
+f = open("README.md", "r")
+lines = f.readlines()
+f.close()
+f = open("README.md", "w")
+for line in lines:
+	if(not line.startswith('<!-- lfm -->')):
+		f.write(line)
+
+for a in albums:
+	f.write(a)
+
+f.close()
+print 'Wrote albums to README'
+
